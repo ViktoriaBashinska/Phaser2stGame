594ba5f2052bb0102f3d0e6fb110d4d68c626791
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-03-05 21:05:57 +0200

    додали керування гравцем

--- game.js
+++ game.js
@@ -80,4 +80,23 @@ function create() {
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
+    //керування гравцем
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-480);
+    }
+}
 }

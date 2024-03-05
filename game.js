01dc08d24bf3547619d399d233b82d8977cacf6a
ViktoriaBashinska <158279310+ViktoriaBashinska@users.noreply.github.com>
2024-03-05 21:00:33 +0200

    додали дерева, коробки та каміння в рандомному порядку

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    parent: game,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,7 +17,7 @@ var config = {
     }
 };
 var player;
-var bomb;
+var bombs;
 var crates;
 var platforms;
 var stars;
@@ -25,7 +26,7 @@ var trees;
 var score = 0;
 var ScoreText;
 var gameOver = false;
-var worldWidth = 5000;
+var worldWidth = 9600;
 var game = new Phaser.Game(config);
 
 function preload() {
@@ -33,6 +34,9 @@ function preload() {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('crate', 'assets/Crate.png');
+    this.load.image('stone', 'assets/Stone.png');
+    this.load.image('tree', 'assets/Tree_3.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
@@ -42,10 +46,24 @@ function create() {
     //додаємо платформи
     platforms = this.physics.add.staticGroup();
     //додаємо землю на всю ширину екрану
-    for (var x = 0; x < worldWidth; x = x + 450){
+    for (var x = 0; x < worldWidth; x = x + 384){
         console.log(x)
-        platforms.create(x, 1080-120, "ground").setOrigin(0, 0).refreshBody();
+        platforms.create(x, 1080-93, "ground").setOrigin(0, 0).refreshBody();
     }
+
+    objects = this.physics.add.staticGroup();
+
+    for (var x = 0; x <= worldWidth; x = x + Phaser.Math.Between(200, 800)){
+        objects.create(x, 987,'crate').setScale(Phaser.Math.FloatBetween(0.5, 2,)).setDepth(Phaser.Math.Between(0, 2)).setOrigin(0, 1).refreshBody();
+        objects.create(x, 987,'stone').setScale(Phaser.Math.FloatBetween(0.5, 2,)).setDepth(Phaser.Math.Between(0, 2)).setOrigin(0, 1).refreshBody();
+        objects.create(x, 989,'tree').setScale(Phaser.Math.FloatBetween(0.5, 2,)).setDepth(Phaser.Math.Between(0, 2)).setOrigin(0, 1).refreshBody();
+    }
+
+
+
+
+
+
     for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(400, 500)) {
         var y = Phaser.Math.FloatBetween(100, 1000)
         console.log(Fx, y)
